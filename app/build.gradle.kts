2778ea622f202157a9ed41186279c0cd222c47f2
Alex <alexapazamendoza@gmail.com>
2024-01-17 16:48:30 -0500

    Add constraint and glide dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Constraint Layout
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:compose:1.0.0-alpha.1")
 }
