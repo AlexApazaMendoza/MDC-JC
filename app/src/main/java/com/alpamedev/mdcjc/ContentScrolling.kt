0f7b8a009d129f7d2cc2ae0a38e3e87052559901
Alex <alexapazamendoza@gmail.com>
2024-01-18 16:10:41 -0500

    Column

--- app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
@@ -1,14 +1,12 @@
 package com.alpamedev.mdcjc
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.alpamedev.mdcjc.ui.theme.MDCJCTheme
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -22,6 +20,12 @@ fun ContentPreview() {
 
 @Composable
 fun Content(modifier: Modifier = Modifier) {
-    Text(text = "Hello World from Compose!",
-        modifier = Modifier.fillMaxSize().padding(16.dp).background(Color.Cyan))
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())  //Estados de la barra de desplazamiento
+    ) {
+        Text(text = "Cursos")
+        Text(text = "Alex")
+        Text(text = "Apaza")
+    }
 }
