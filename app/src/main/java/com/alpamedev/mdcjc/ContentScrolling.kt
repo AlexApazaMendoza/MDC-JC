9ecb221977a22278917451ee71f79433917447b4
Alex <alexapazamendoza@gmail.com>
2024-02-01 16:59:22 -0500

    Text

--- app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.MaterialTheme
 import com.alpamedev.mdcjc.ui.theme.MDCJCTheme
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -46,6 +49,13 @@ fun Content(modifier: Modifier = Modifier) {
                         .background(color = colorResource(id = R.color.teal_200))
                         .fillMaxWidth()
                         .height(dimensionResource(id = R.dimen.card_img_cover_height))
+                )
+                Text(
+                    text = stringResource(id = R.string.card_title),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(dimensionResource(id = R.dimen.common_padding_default)),
+                    style = MaterialTheme.typography.title3
                 )
             }
         }
