c813304f5cefe0c90d3477c21dd580674f3b856f
Alex <alexapazamendoza@gmail.com>
2024-02-02 10:22:22 -0500

    Trailing icon

--- app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
@@ -2,6 +2,7 @@ package com.alpamedev.mdcjc
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +29,8 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Checkbox
@@ -94,7 +100,18 @@ fun Content(modifier: Modifier = Modifier) {
                             top = dimensionResource(id = R.dimen.common_padding_default),
                             start = dimensionResource(id = R.dimen.common_padding_default),
                             end = dimensionResource(id = R.dimen.common_padding_default)
-                        )
+                        ),
+                    trailingIcon = {
+                        if (urlValue.isNotEmpty()) {
+                            Icon(
+                                imageVector = Icons.Filled.Clear,
+                                contentDescription = "Clear",
+                                modifier = Modifier.clickable {
+                                    urlValue = ""
+                                }
+                            )
+                        }
+                    }
                 )
                 Text(
                     text = stringResource(id = R.string.card_required),
@@ -113,6 +130,9 @@ fun Content(modifier: Modifier = Modifier) {
                 var isCheckboxEnabled by remember {
                     mutableStateOf(false)
                 }
+                var isPasswordVisible by remember {
+                    mutableStateOf(false)
+                }
                 OutlinedTextField(
                     value = passwordValue,
                     onValueChange = { passwordValue = it },
@@ -125,7 +145,17 @@ fun Content(modifier: Modifier = Modifier) {
                             start = dimensionResource(id = R.dimen.common_padding_default),
                             end = dimensionResource(id = R.dimen.common_padding_default)
                         ),
-                    enabled = isCheckboxEnabled
+                    enabled = isCheckboxEnabled,
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        Icon(
+                            painter = if (isPasswordVisible) painterResource(id = R.drawable.ic_visibility) else painterResource(id = R.drawable.ic_disabled_visible),
+                            contentDescription = null,
+                            modifier = Modifier.clickable {
+                                isPasswordVisible = !isPasswordVisible
+                            }
+                        )
+                    }
                 )
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Checkbox(
