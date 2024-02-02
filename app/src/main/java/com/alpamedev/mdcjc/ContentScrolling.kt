db96823345171c184daa9490093f9c9c33918050
Alex <alexapazamendoza@gmail.com>
2024-02-02 10:06:45 -0500

    Checkbox

--- app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
@@ -3,6 +3,7 @@ package com.alpamedev.mdcjc
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -25,6 +27,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Checkbox
 import androidx.wear.compose.material.ContentAlpha
 import androidx.wear.compose.material.MaterialTheme
 import com.alpamedev.mdcjc.ui.theme.MDCJCTheme
@@ -104,6 +107,35 @@ fun Content(modifier: Modifier = Modifier) {
                             top = dimensionResource(id = R.dimen.common_padding_min)
                         )
                 )
+                var passwordValue by remember {
+                    mutableStateOf("")
+                }
+                var isCheckboxEnabled by remember {
+                    mutableStateOf(false)
+                }
+                OutlinedTextField(
+                    value = passwordValue,
+                    onValueChange = { passwordValue = it },
+                    label = { Text(text = stringResource(id = R.string.card_password)) },
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = dimensionResource(id = R.dimen.common_padding_default),
+                            start = dimensionResource(id = R.dimen.common_padding_default),
+                            end = dimensionResource(id = R.dimen.common_padding_default)
+                        ),
+                    enabled = isCheckboxEnabled
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = isCheckboxEnabled,
+                        onCheckedChange = {
+                            isCheckboxEnabled = it
+                        }
+                    )
+                    Text(text = stringResource(id = R.string.card_enable_pass))
+                }
             }
         }
     }
