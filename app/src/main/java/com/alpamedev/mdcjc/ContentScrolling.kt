e20f993a2e20d24ea52472c0507023642028fac3
Alex <alexapazamendoza@gmail.com>
2024-02-04 12:01:50 -0500

    Views for ConstraintLayout

--- app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
+++ app/src/main/java/com/alpamedev/mdcjc/ContentScrolling.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -34,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -42,8 +44,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Checkbox
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.ChipBorder
@@ -70,6 +77,35 @@ fun Content(modifier: Modifier = Modifier) {
         modifier = modifier
             .verticalScroll(rememberScrollState())  //Estados de la barra de desplazamiento
     ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column {
+                val image = ContextCompat.getDrawable(LocalContext.current, R.drawable.ic_store)
+                Image(bitmap = image!!.toBitmap().asImageBitmap(), contentDescription = null)
+                Button(onClick = { /*TODO*/ }) {
+                    Icon(painter = painterResource(id = R.drawable.ic_store), contentDescription = null)
+                    Text(text = stringResource(id = R.string.card_btn_buy))
+                }
+                TextButton(onClick = { /*TODO*/ }) {
+                    Text(text = stringResource(id = R.string.card_btn_skip))
+                }
+                Text(
+                    text = stringResource(id = R.string.card_title),
+                    style = MaterialTheme.typography.title1,
+                    textAlign = TextAlign.Start
+                )
+                Text(
+                    text = stringResource(id = R.string.large_text),
+                    style = MaterialTheme.typography.body1,
+                    textAlign = TextAlign.Start,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
         var colorMain by remember {
             mutableStateOf(Color.LightGray)
         }
